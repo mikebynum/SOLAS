f4701e8c31e927b6e99cfed56a02f7c9ec9c6720
Mike Bynum <mikebynum@outlook.com>
2017-07-30 18:40:09 -0500

    More SOLAS commits

---
+++ SOLAS/Controllers/HelpController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOLAS.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Volunteer()
+        {
+            return View();
+        }
+        public ActionResult Foster()
+        {
+            return View();
+        }
+        public ActionResult WantsList()
+        {
+            return View();
+        }
+    }
+}
--- SOLAS/Controllers/HomeController.cs
+++ SOLAS/Controllers/HomeController.cs
@@ -54,5 +54,6 @@ namespace SOLAS.Controllers
 
             return View();
         }
+
     }
 }
